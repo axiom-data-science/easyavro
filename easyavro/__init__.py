99aef9b120243298220e73b03dc8d75d6c4992b5
Kyle Wilcox <kyle@axiomdatascience.com>
2018-09-11 13:44:24 -0400

    Cleanup Dockerfile and use an environment.yml over requirements*.txt

--- easyavro/__init__.py
+++ easyavro/__init__.py
@@ -4,3 +4,10 @@ from .consumer import EasyAvroConsumer
 from .producer import EasyAvroProducer, schema
 
 __version__ = "2.1.0"
+
+
+__all__ = [
+    'EasyAvroConsumer',
+    'EasyAvroProducer',
+    'schema'
+]
