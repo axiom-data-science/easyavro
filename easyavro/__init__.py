d20d09b2f204987c1f0e2f3fbd8a9369345a636c
Kyle Wilcox <kyle@axiomdatascience.com>
2021-03-08 16:39:43 -0500

    Add a MultiConsumer for grabbing many message at once

--- easyavro/__init__.py
+++ easyavro/__init__.py
@@ -1,6 +1,7 @@
 #!python
 # coding=utf-8
 from .consumer import EasyAvroConsumer, EasyConsumer
+from .multi_consumer import EasyMultiConsumer, EasyMultiAvroConsumer
 from .producer import EasyAvroProducer, EasyProducer, schema
 
 __version__ = "3.0.0"
@@ -9,6 +10,8 @@ __version__ = "3.0.0"
 __all__ = [
     'EasyConsumer',
     'EasyAvroConsumer',
+    'EasyMultiConsumer',
+    'EasyMultiAvroConsumer',
     'EasyProducer',
     'EasyAvroProducer',
     'schema'
